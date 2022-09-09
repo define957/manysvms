521240cd4df60aa08978adae96816c99bc921218
Morii <zhangjiaqi957957@outlook.com>
2022-09-10 01:13:55 +0800

    add glass dataset

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Glass Identification Data Set (UCI dataset)
+#'
+#' From USA Forensic Science Service; 6 types of glass;
+#' defined in terms of their oxide content (i.e. Na, Fe, K, etc)
+#'
+#' @format A data frame with 214 rows and 10 variables:
+#' \describe{
+#'   \item{RI}{refractive index}
+#'   \item{Na}{Sodium (unit measurement: weight percent in corresponding
+#'   oxide, as are attributes 4-10)}
+#'   \item{Mg}{Magnesium}
+#'   \item{Al}{Aluminum}
+#'   \item{Si}{Silicon}
+#'   \item{K}{Potassium}
+#'   \item{Ca}{Calcium}
+#'   \item{Ba}{Barium}
+#'   \item{Fe}{Iron}
+#'   \item{glass.type}{Type of glass}
+#'
+#' }
+"glass"
