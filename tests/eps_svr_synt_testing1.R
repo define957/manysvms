5a9b0ee9ee814c7d22338de0cac46d027cc7b602
Morii <zhangjiaqi957957@outlook.com>
2022-09-10 23:16:48 +0800

    update test file

--- tests/eps_svr_synt_testing1.R
+++ tests/eps_svr_synt_testing1.R
@@ -1,17 +1,18 @@
-#library(manysvms)
+library(manysvms)
 library(MASS)
 library(ggplot2)
 
 set.seed(1235)
-x1 <- seq(-5, 5, length.out = 3000)#mvrnorm(200, mu = 0, Sigma = 0.1)
-x2 <- sin(x1)  + mvrnorm(3000, mu = 0, Sigma = 0.03)
+obs <- 200
+x1 <- seq(-5, 5, length.out = obs)
+x2 <- sin(x1)  + mvrnorm(obs, mu = 0, Sigma = 0.03)
 
 X <- as.matrix(x1)
 y <- as.matrix(x2)
 
 s <- Sys.time()
-m <- eps.svr(X,y, eps=0.1, kernel = 'rbf', C = 1,
-             gamma = 1, max.steps = 500, rcpp = FALSE)
+m <- eps.svr(X,y, eps=0.3, kernel = 'rbf', C = 1,
+             gamma = 1, max.steps = 500, rcpp = TRUE)
 e <- Sys.time()
 print(e - s)
 dataXy <- as.data.frame(cbind(X, y))
@@ -19,6 +20,6 @@ dataXy <- as.data.frame(cbind(X, y))
 ggplot(data = dataXy, aes(x = X, y = y))+
   geom_point()+
   geom_line(aes(x=X, y=m$fitted))+
-  geom_line(aes(x=X, y=m$fitted + m$epsilon, color = 'red'))+
-  geom_line(aes(x=X, y=m$fitted - m$epsilon, color = 'red'))+
-  theme_classic()
+  geom_line(aes(x=X, y=m$fitted + m$epsilon, color = 'red'), show.legend = FALSE)+
+  geom_line(aes(x=X, y=m$fitted - m$epsilon, color = 'red'), show.legend = FALSE)+
+  theme_bw()
