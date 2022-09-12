0ec094d44fd9862ede6f5914002b63739fd49382
ZHANG Jiaqi <53004538+define957@users.noreply.github.com>
2022-09-12 14:27:35 +0800

    Update kernel_functions.R

--- R/kernel_functions.R
+++ R/kernel_functions.R
@@ -7,3 +7,8 @@ rbf_kernel <- function(x1, x2, gamma= 1){
   K <-  exp(- gamma * norm(temp, type = '2')^2)
   return(K)
 }
+
+poly_kernel <- function(x1, x2, gamma, degree = 3, coef0 = 0){
+  K <- (gamma * x1 %*% t(x2) + coef0)^(degree)
+  return(K)
+}
