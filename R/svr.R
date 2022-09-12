b861b5e11120f2a15a2896d1247bd3817efe4801
Morii <zhangjiaqi957957@outlook.com>
2022-09-12 15:28:40 +0800

    update eps svr example

--- R/svr.R
+++ R/svr.R
@@ -22,20 +22,26 @@
 #' library(ggplot2)
 #'
 #' set.seed(1235)
-#' x1 <- mvrnorm(200, mu = 0, Sigma = 3)
-#' x2 <- 2*x1  + mvrnorm(200, mu = 0, Sigma = 1)
+#' obs <- 200
+#' x1 <- seq(-5, 5, length.out = obs)
+#' x2 <- sin(x1)  + mvrnorm(obs, mu = 0, Sigma = 0.03)
 #'
 #' X <- as.matrix(x1)
 #' y <- as.matrix(x2)
 #'
-#' m <- eps.svr(X,y, eps=2, C = 1)
+#' s <- Sys.time()
+#' m <- eps.svr(X,y, eps=0.3, kernel = 'rbf', C = 1,
+#'              gamma = 1, max.steps = 500, rcpp = TRUE)
+#' e <- Sys.time()
+#' print(e - s)
 #' dataXy <- as.data.frame(cbind(X, y))
+#'
 #' ggplot(data = dataXy, aes(x = X, y = y))+
 #'   geom_point()+
-#'   geom_abline(slope = m$coef, intercept = m$intercept)+
-#'   geom_abline(slope = m$coef, intercept = m$intercept + m$epsilon, linetype=2, color = 'red')+
-#'   geom_abline(slope = m$coef, intercept = m$intercept - m$epsilon, linetype=2, color = 'red')+
-#'   theme_classic()
+#'   geom_line(aes(x=X, y=m$fitted))+
+#'   geom_line(aes(x=X, y=m$fitted + m$epsilon, color = 'red'), show.legend = FALSE)+
+#'   geom_line(aes(x=X, y=m$fitted - m$epsilon, color = 'red'), show.legend = FALSE)+
+#'   theme_bw()
 
 eps.svr <- function(X, y, eps = 0.1,
                     kernel = c('linear', 'rbf', 'poly'),
