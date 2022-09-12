93f91fb9cf586716fbbc4d149f153adf13fa30b9
ZHANG Jiaqi <53004538+define957@users.noreply.github.com>
2022-09-12 14:27:06 +0800

    Update kernel_functions.cpp

--- src/kernel_functions.cpp
+++ src/kernel_functions.cpp
@@ -12,11 +12,19 @@ SEXP cpp_rbf_kernel(arma::mat x1, arma::mat x2, double gamma)
   int j = 0;
   arma::mat kernelx(n, m);
   for(i = 0; i < n; i ++){
-    for(j = 0; j < m; j ++){
+    for(j = 0; j < n; j ++){
       kernelx(i, j) = exp(as_scalar(- gamma * pow(norm(x1.row(i) - x2.row(j),2), 2)));
     }
   }
   return Rcpp::wrap(kernelx);
 }
+
+//[[Rcpp::export]]
+SEXP cpp_poly_kernel(arma::mat x1, arma::mat x2, double gamma, int degree, double coef0)
+{
+  arma::mat kernelx = arma::pow((gamma * x1 * x2.t() + coef0), degree);
+  return Rcpp::wrap(kernelx);
+}
+
 
 
