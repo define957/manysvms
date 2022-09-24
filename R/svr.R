c49b84bc41fc5574b8ecf9e0ce7126ee3649b9fb
Morii <zhangjiaqi957957@outlook.com>
2022-09-24 15:30:29 +0800

    update svr document

--- R/svr.R
+++ R/svr.R
@@ -25,13 +25,13 @@
 #' set.seed(1235)
 #' obs <- 200
 #' x1 <- seq(-5, 5, length.out = obs)
-#' x2 <- sin(x1)  + mvrnorm(obs, mu = 0, Sigma = 0.03)
+#' x2 <- sin(x1) + mvrnorm(obs, mu = 0, Sigma = 0.03)
 #'
 #' X <- as.matrix(x1)
 #' y <- as.matrix(x2)
 #'
 #' s <- Sys.time()
-#' m <- eps.svr(X,y, eps=0.3, kernel = 'rbf', C = 1,
+#' m <- eps.svr(X, y, eps=0.3, kernel = 'rbf', C = 1,
 #'              gamma = 1, max.steps = 500, rcpp = TRUE)
 #' e <- Sys.time()
 #' print(e - s)
