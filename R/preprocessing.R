63d14ee0c44a8141cbd241dd27047ce2ca18b359
Morii <zhangjiaqi957957@outlook.com>
2022-12-17 21:02:00 +0800

    add noisy_label_generator

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -31,3 +31,34 @@ train_test_split <- function(X, y, test_size = 0.3,
                           "test_X" = test_X, "test_y" = test_y)
   return(train_test_data)
 }
+
+
+#' Generate noisy label
+#'
+#' @author Zhang Jiaqi
+#' @param y clean label.
+#' @param p ratio of noisy.
+#' @param seed random seed.
+#' @export
+
+noisy_label_generator <- function(y, p, seed = NULL){
+  if (is.null(seed) == FALSE) {
+    set.seed(seed)
+  }
+  y <- as.matrix(y)
+  class_set <- unique(y)
+  class_num <- length(class_set)
+  class_idx <- list()
+  for (i in 1:class_num) {
+    idx <- which(y == class_set[i])
+    class_idx[[i]] <- idx
+  }
+  for (i in 1:class_num) {
+    m <- length(class_idx[[i]])
+    n <- round(m*p, 0)
+    idx_temp <- sample(m, n)
+    noisy_y <- sample(class_set[class_set != class_set[i]], n, replace = T)
+    y[class_idx[[i]][idx_temp]] <- noisy_y
+  }
+  return(y)
+}
