429a7dd7b126fc46b7703378fc53f7b5ab5cc6f1
define957 <zhangjiaqi597597@outlook.com>
2023-02-24 16:06:13 +0800

    update document of pegasos

--- R/pegasos.R
+++ R/pegasos.R
@@ -1,3 +1,13 @@
+#  Primal Estimated sub-Gradient SOlver for SVM (Pegasos)
+#'
+#' @author Zhang Jiaqi.
+#' @param X,y dataset and label.
+#' @param w initial point.
+#' @param max.steps the number of iterations to solve the optimization problem.
+#' @param fx sub-gradient of objective function.
+#' @return return optimal solution.
+#' @export
+#' @examples
 pegasos <- function(X, y, w, m, max.steps, fx, seed, ...) {
   if (is.null(seed)==FALSE) {
     set.seed(seed)
