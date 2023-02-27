525f265b0a49291699c47ae9ae1ca54df03e1dcc
LiFeiH <lifh13398309563@163.com>
2023-02-27 21:17:22 +0800

    update cross_validation

--- R/model_selection.R
+++ R/model_selection.R
@@ -15,25 +15,21 @@ cross_validation <- function(model, X, y, K = 5, metric, predict_func = predict,
                              shuffle = TRUE, seed = NULL, ...) {
   X <- as.matrix(X)
   y <- as.matrix(y)
-  n <- nrow(X)
-  v_size <- n %/% K
+  index = c(0:K)*nrow(data)/K
   if (is.null(seed) ==FALSE) {
     set.seed(seed)
   }
-
   if (shuffle == TRUE) {
     idx <- sample(n)
     X <- X[idx, ]
     y <- y[idx]
   }
   metric_mat <- matrix(0, nrow = 1, ncol = K)
-  idx_cv <- 1
   for (i in 1:K) {
-    v_idx <- idx_cv:(idx_cv+v_size-1)
-    X_test <- X[v_idx, ]
-    y_test <- y[v_idx]
-    X_train <- X[-v_idx, ]
-    y_train <- y[-v_idx]
+    X_test <- X[c(index[i]:index[i+1]), ]
+    y_test <- y[c(index[i]:index[i+1])]
+    X_train <- X[-c(index[i]:index[i+1]), ]
+    y_train <- y[-c(index[i]:index[i+1])]
     model_res <- do.call("model", list("X" = X_train, "y" = y_train, ...))
     y_test_hat <- predict_func(model_res, X_test)
     metric_mat[i] <- metric(y_test, y_test_hat)
@@ -75,21 +71,18 @@ grid_search_cv <- function(model, X, y, K = 5, metric, param_list,
   doSNOW::registerDoSNOW(cl)
   i <- 1
   cv_res <- foreach::foreach(i = 1:n_param, .combine = rbind,
-                          .packages = c('manysvms', 'Rcpp'),
-                          .options.snow = opts) %dopar% {
-
-  # for (i in 1:n_param) {
-    temp <- data.frame(param_grid[i, ])
-    colnames(temp) <- param_names
-    params_cv <- append(list("model" = model,
-                             "X" = X, "y" = y, "K" = K,
-                             "metric" = metric,
-                             "predict_func" =  predict_func,
-                             "shuffle" = shuffle, "seed" = seed, ...),
-                             temp)
-    cv_res <- do.call("cross_validation", params_cv)
-
-  }
+                             .packages = c('manysvms', 'Rcpp'),
+                             .options.snow = opts) %dopar% {
+                               temp <- data.frame(param_grid[i, ])
+                               colnames(temp) <- param_names
+                               params_cv <- append(list("model" = model,
+                                                        "X" = X, "y" = y, "K" = K,
+                                                        "metric" = metric,
+                                                        "predict_func" =  predict_func,
+                                                        "shuffle" = shuffle, "seed" = seed, ...),
+                                                   temp)
+                               cv_res <- do.call("cross_validation", params_cv)
+                             }
   close(pb)
   parallel::stopCluster(cl)
   return(cv_res)
