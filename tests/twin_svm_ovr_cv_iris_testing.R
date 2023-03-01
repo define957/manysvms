95f56dd13186fbae5bca18e994f4c23e16b1f5fb
define957 <zhangjiaqi957957@outlook.com>
2023-03-01 20:32:20 +0800

    update test file

--- tests/twin_svm_ovr_cv_iris_testing.R
+++ tests/twin_svm_ovr_cv_iris_testing.R
@@ -6,5 +6,5 @@ X <- iris[, 1:4]
 y <- iris[, 5]
 gamma <- c(8, 4, 2, 1, 1/4)
 
-res <- cv.twinsvm_ovr(X, y, K = 5, kernel = 'rbf',
+res <- cv.twinsvm_ovr(X, y, K = 5, kernel = 'rbf', reg = 1e-3,
                       gamma = gamma, threads.num = 2)
