7f0ceb9456cd40d14a6629e3fa17704542e97b11
define957 <zhangjiaqi597597@outlook.com>
2023-03-12 13:27:51 +0800

    add least squares support vector machine

---
+++ R/ls_svm.R
@@ -0,0 +1,118 @@
+ls_svm_dual_solver <- function (KernelX, y, C = 1,
+                                   eps = 1e-5, max.steps = 80, rcpp = TRUE) {
+  D <- diag(as.vector(y))
+  n <- nrow(KernelX)
+  H <- D %*% KernelX %*% D
+  m <- nrow(X)
+  alphas <- solve(H + diag(rep(1/C, m)), matrix(1, nrow = m))
+  coef <- D %*% alphas
+  BaseDualLeastSquaresSVMClassifier <- list(coef = as.matrix(coef))
+  class(BaseDualLeastSquaresSVMClassifier) <- "BaseDualLeastSquaresSVMClassifier"
+  return(BaseDualLeastSquaresSVMClassifier)
+}
+
+
+ls_svm_primal_solver <- function (KernelX, y, C = 1, eps = 1e-5,
+                                     max.steps = 80, batch_size = nrow(KernelX) / 10,
+                                     seed = NULL, sample_seed = NULL,
+                                     optimizer = pegasos, ...) {
+   gLeastSquares <- function(KernelX, y, v, ...) { # gradient of Least Squares loss function
+    C <- list(...)$C
+    xn <- nrow(KernelX)
+    xp <- ncol(KernelX)
+    sg <- matrix(0, nrow = xp, ncol = 1)
+    u <- 1 - y * (KernelX %*% v)
+    sg <- v - (C/xn) * t(KernelX)%*%(u*y)
+    return(sg)
+  }
+  xn <- nrow(KernelX)
+  xp <- ncol(KernelX)
+  w0 <- matrix(0, nrow = xp, ncol = 1)
+  wt <- optimizer(KernelX, y, w0, batch_size, max.steps, gLeastSquares, sample_seed, C = C, ...)
+  wnorm <- norm(wt[1:xp], type = "2")
+  BasePrimalLeastSquaresSVMClassifier <- list(coef = as.matrix(wt[1:xp]))
+  class(BasePrimalLeastSquaresSVMClassifier) <- "BasePrimalLeastSquaresSVMClassifier"
+  return(BasePrimalLeastSquaresSVMClassifier)
+}
+
+
+#' Least Squares Support Vector Machine
+#'
+#' \code{ls_svm} is an R implementation of Hinge-SVM
+#'
+#' @author Zhang Jiaqi.
+#' @param X,y dataset and label.
+#' @param C plenty term.
+#' @param kernel kernel function. The definitions of various kernel functions are as follows:
+#' \describe{
+#'     \item{linear:}{\eqn{u'v}{u'*v}}
+#'     \item{poly:}{\eqn{(\gamma u'v + coef0)^{degree}}{(gamma*u'*v + coef0)^degree}}
+#'     \item{rbf:}{\eqn{e^{(-\gamma |u-v|^2)}}{exp(-gamma*|u-v|^2)}}
+#' }
+#' @param gamma parameter for \code{'rbf'} and \code{'poly'} kernel. Default \code{gamma = 1/ncol(X)}.
+#' @param degree parameter for polynomial kernel, default: \code{degree = 3}.
+#' @param coef0 parameter for polynomial kernel,  default: \code{coef0 = 0}.
+#' @param eps the precision of the optimization algorithm.
+#' @param max.steps the number of iterations to solve the optimization problem.
+#' @param batch_size mini-batch size for primal solver.
+#' @param solver \code{"dual"} and \code{"primal"} are available.
+#' @param rcpp speed up your code with Rcpp, default \code{rcpp = TRUE}.
+#' @param fit_intercept if set \code{fit_intercept = TRUE},
+#'                      the function will evaluates intercept.
+#' @param optimizer default primal optimizer pegasos.
+#' @param randx parameter for reduce SVM, default \code{randx = 0.1}.
+#' @param ... unused parameters.
+#' @return return \code{SVMClassifier} object.
+#' @export
+ls_svm <- function (X, y, C = 1, kernel = c("linear", "rbf", "poly"),
+                    gamma = 1 / ncol(X), degree = 3, coef0 = 0,
+                    eps = 1e-5, max.steps = 80, batch_size = nrow(X) / 10,
+                    solver = c("dual", "primal"), rcpp = TRUE,
+                    fit_intercept = TRUE, optimizer = pegasos, randx = 0.1, ...) {
+  X <- as.matrix(X)
+  y <- as.matrix(y)
+  class_set <- unique(y)
+  idx <- which(y == class_set[1])
+  y[idx] <- 1
+  y[-idx] <- -1
+  if (length(class_set) > 2) {
+    stop("The number of class should less 2!")
+  }
+  kernel <- match.arg(kernel)
+  solver <- match.arg(solver)
+  if (fit_intercept == TRUE) {
+    X <- cbind(X, 1)
+  }
+  if (kernel == "linear" & solver == "primal") {
+    KernelX <- X
+  } else if (kernel != "linear" & solver == "primal"){
+    if (randx > 0) {
+      randX = X[sample(nrow(X), floor(randx*nrow(X))),]
+    }
+    KernelX <- kernel_function(X, randX,
+                               kernel.type = kernel,
+                               gamma = gamma, degree = degree, coef0 = coef0,
+                               rcpp = rcpp)
+    X <- randX
+  } else if (solver == "dual"){
+    KernelX <- kernel_function(X, X,
+                               kernel.type = kernel,
+                               gamma = gamma, degree = degree, coef0 = coef0,
+                               rcpp = rcpp)
+  }
+  if (solver == "primal") {
+    solver.res <- ls_svm_primal_solver(KernelX, y, C, eps,
+                                       max.steps, batch_size,
+                                       optimizer, ...)
+  } else if(solver == "dual") {
+    solver.res <- ls_svm_dual_solver(KernelX, y, C, rcpp)
+  }
+  SVMClassifier <- list("X" = X, "y" = y, "class_set" = class_set,
+                        "C" = C, "kernel" = kernel,
+                        "gamma" = gamma, "degree" = degree, "coef0" = coef0,
+                        "solver" = solver, "coef" = solver.res$coef,
+                        "fit_intercept" = fit_intercept,
+                        "rcpp" = rcpp)
+  class(SVMClassifier) <- "SVMClassifier"
+  return(SVMClassifier)
+}
---
+++ tests/ls_svm_synt_testing_1.R
@@ -0,0 +1,67 @@
+library(MASS)
+library(ggplot2)
+library(manysvms)
+
+set.seed(100)
+n <- 150
+X1 <- mvrnorm(n, mu = c(-3, -3), Sigma = diag(1, nrow = 2))
+X2 <- mvrnorm(n, mu = c(3, 3), Sigma = diag(1, nrow = 2))
+X <- rbind(X1, X2)
+
+y <- rep(c(1, 2), c(n, n))
+
+model1 <- ls_svm(X, y, C = 0.5, epsilon = 0.5, max.steps = 500)
+w <- t(cbind(X, 1)) %*% model1$coef
+dataXy <- data.frame(X, y)
+dataXy$y <- as.factor(dataXy$y)
+
+ggplot(dataXy, aes(x = X1, y = X2, color = y)) +
+  geom_point() +
+  geom_abline(slope = -w[1] / w[2],
+              intercept = -w[3] / w[2]) +
+  geom_abline(slope = -w[1] / w[2],
+              intercept = -(-1 + w[3])/w[2]) +
+  geom_abline(slope = -w[1] / w[2],
+              intercept = -(1 + w[3])/w[2]) +
+  theme_bw()
+
+cat(w, "\n")
+
+s <- Sys.time()
+model1 <- ls_svm(X, y, C = 1, epsilon = 0.5, batch_size = 150,
+                      solver = "primal", max.steps = 500)
+e <- Sys.time()
+print(e - s)
+dataXy <- data.frame(X, y)
+dataXy$y <- as.factor(dataXy$y)
+
+ggplot(dataXy, aes(x = X1, y = X2, color = y)) +
+  geom_point() +
+  geom_abline(slope = -model1$coef[1] / model1$coef[2],
+              intercept = -model1$coef[3]/model1$coef[2]) +
+  geom_abline(slope = -model1$coef[1] / model1$coef[2],
+              intercept = -(-1 + model1$coef[3])/model1$coef[2]) +
+  geom_abline(slope = -model1$coef[1] / model1$coef[2],
+              intercept = -(1 + model1$coef[3])/model1$coef[2]) +
+  theme_bw()
+
+cat(model1$coef, "\n")
+
+lambda <- seq(1, 6)
+epsilon <- c(0, 0.1, 0.2, 0.4)
+C <- matrix(0, nrow = 7)
+for (i in -3:3) {
+  C[i] <- 2^(i)
+}
+param_list <- list("C" = C, "gamma " = C,
+                   "lambda" = c(1, 2, 3),
+                   "epsilon" = 0)
+
+s <- Sys.time()
+grid_search_cv(ls_svm, X, y, metric = accuracy,
+               param_list = param_list, seed = 1234, K = 5,
+               max.steps = 500, threads.num = 2,
+               solver = "primal", randx = 0.1, batch_size = 1,
+               kernel = "rbf")
+e <- Sys.time()
+print(e - s)
