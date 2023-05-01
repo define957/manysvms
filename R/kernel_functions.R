8722a4b8942aef4853c025256dd77124888549ed
define957 <zhangjiaqi957957@outlook.com>
2023-05-01 16:08:42 +0800

    upload

--- R/kernel_functions.R
+++ R/kernel_functions.R
@@ -7,20 +7,28 @@ r_poly_kernel <- function(x1, x2, gamma, degree = 3, coef0 = 0){
   return(K)
 }
 
-r_rbf_kernel <- function(x1, x2, gamma= 1/ncol(x2)){
-  m <- nrow(x1)
-  n <- nrow(x2)
-  K <- matrix(0, nrow = m, ncol = n)
-  for(i in 1:m){
-    for(j in 1:n){
-      temp <- as.matrix(x1[i, ] - x2[j, ])
-      K[i, j] <- exp(- gamma * norm(temp, type = '2')^2)
-    }
+r_rbf_kernel <- function(x1, x2, gamma= 1/ncol(x2), symmetric = FALSE) {
+  x1 <- as.matrix(x1)
+  x2 <- as.matrix(x2)
+
+  n1 <- nrow(x1)
+  norms1 <- as.matrix(apply(x1^2, 1, sum))
+  e1 <- matrix(1, 1, n2)
+  if (symmetric == FALSE) {
+    n2 <- nrow(x2)
+    norms2 <- as.matrix(apply(x2^2, 1, sum))
+    e2 <- matrix(1, 1, n1)
+  } else {
+    n2 <- n1
+    norms2 <- norms1
+    e2 <- e1
   }
+  K <- exp(gamma*(-norms1%*%e1 - t(e2)%*% t(norms2) + 2*(x1%*%t(x2))))
   return(K)
 }
 
-rbf_kernel <- function(x1, x2, gamma = 1/ncol(x1), rcpp = TRUE){
+rbf_kernel <- function(x1, x2, gamma = 1/ncol(x1),
+                       symmetric = FALSE, rcpp = TRUE){
   if(rcpp == TRUE){
     K <- cpp_rbf_kernel(x1, x2, gamma)
   }else if(rcpp == FALSE){
@@ -38,18 +46,19 @@ rbf_kernel <- function(x1, x2, gamma = 1/ncol(x1), rcpp = TRUE){
 #' @param gamma parameter for \code{'rbf'} and \code{'poly'} kernel. Default \code{gamma = 1/ncol(X)}.
 #' @param degree parameter for polynomial kernel, default: \code{degree = 3}.
 #' @param coef0 parameter for polynomial kernel,  default: \code{coef0 = 0}.
+#' @param symmetric if \code{x1 == x2} you can set \code{symmetric == TRUE}.
 #' @param rcpp speed up your code with Rcpp, default \code{rcpp = TRUE}.
 #' @export
 
 kernel_function <- function(x1, x2,
                             kernel.type = c('linear', 'rbf', 'poly'),
                             gamma = 1/ncol(x1), degree = 3, coef0 = 0,
-                            rcpp = TRUE){
+                            symmetric = FALSE, rcpp = TRUE){
   kernel.type <- match.arg(kernel.type)
   if(kernel.type == 'linear'){
     K <- r_linear_kernel(x1, x2)
   }else if(kernel.type == 'rbf'){
-    K <- rbf_kernel(x1, x2, gamma, rcpp)
+    K <- rbf_kernel(x1, x2, gamma, symmetric, rcpp)
   }else if(kernel.type == 'poly'){
     K <- r_poly_kernel(x1, x2, gamma, degree = 3, coef0 = 0)
   }
