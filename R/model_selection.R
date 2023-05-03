32f1097d8fcc018d783c854ec93a600c112e91cd
define957 <zhangjiaqi597597@outlook.com>
2023-05-03 13:28:59 +0800

    update cross_validation

--- R/model_selection.R
+++ R/model_selection.R
@@ -15,21 +15,22 @@ cross_validation <- function(model, X, y, K = 5, metric, predict_func = predict,
   y <- as.matrix(y)
   n <- nrow(X)
   metric_mat <- matrix(0, nrow = 1, ncol = K)
-  index <- c(0:K)*n/K
+  index <- sort(rep(1:K, length.out = n))
   for (i in 1:K) {
-    X_test <- X[c(index[i]:index[i + 1]), ]
-    y_test <- y[c(index[i]:index[i + 1])]
+    idx <- which(index == i)
+    X_test <- X[idx, ]
+    y_test <- y[idx]
     if(K == 1){
-      X_train = X_test
-      y_train = y_test
+      X_train <- X_test
+      y_train <- y_test
     }else{
-      X_train <- X[-c(index[i]:index[i + 1]), ]
-      y_train <- y[-c(index[i]:index[i + 1])]
+      X_train <- X[-idx, ]
+      y_train <- y[-idx]
     }
     model_res <- do.call("model", list("X" = X_train, "y" = y_train, ...))
     y_test_hat <- predict_func(model_res, X_test, ...)
     metric_mat[i] <- metric(y_test, y_test_hat)
-  }  
+  }
   return(metric_mat)
 }
 
@@ -236,18 +237,18 @@ cross_validation_noisy <- function(model, X, y, y_noisy, K = 5, metric, predict_
   y <- as.matrix(y)
   y_noisy <- as.matrix(y_noisy)
   n <- nrow(X)
-  index <- c(0:K)*n/K
-  
   metric_mat <- matrix(0, nrow = 1, ncol = K)
+  index <- sort(rep(1:K, length.out = n))
   for (i in 1:K) {
-    X_test <- X[c(index[i]:index[i + 1]), ]
-    y_test <- y[c(index[i]:index[i + 1])]
+    idx <- which(index == i)
+    X_test <- X[idx, ]
+    y_test <- y[idx]
     if(K == 1){
-      X_train = X_test
-      y_train = y_test
+      X_train <- X_test
+      y_train <- y_test
     }else{
-      X_train <- X[-c(index[i]:index[i + 1]), ]
-      y_train <- y_noisy[-c(index[i]:index[i + 1])]
+      X_train <- X[-idx, ]
+      y_train <- y[-idx]
     }
     model_res <- do.call("model", list("X" = X_train, "y" = y_train, ...))
     y_test_hat <- predict_func(model_res, X_test, ...)
