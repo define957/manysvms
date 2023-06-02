791330a00d77d025939bffd46085502796f64860
define957 <zhangjiaqi957957@outlook.com>
2023-06-02 10:47:55 +0800

    update pegasos

--- R/pegasos.R
+++ R/pegasos.R
@@ -6,11 +6,12 @@
 #' @param m mini-batch size for pegasos solver.
 #' @param max.steps the number of iterations to solve the optimization problem.
 #' @param fx sub-gradient of objective function.
+#' @param eps the precision of the optimization algorithm.
 #' @param ... additional settings for the sub-gradient.
 #' @return return optimal solution.
 #' @references ${1:Pegasos: Primal Estimated sub-GrAdient SOlver for SVM}
 #' @export
-pegasos <- function(X, y, w, m, max.steps, fx, ...) {
+pegasos <- function(X, y, w, m, max.steps, fx, eps = 1e-5, ...) {
   C <- list(...)$C
   v <- w
   nx = nrow(X)
@@ -24,6 +25,11 @@ pegasos <- function(X, y, w, m, max.steps, fx, ...) {
     dF <- fx(xm, ym, v, At = At, ...)
     v <- v - (C/t)*dF
     v <- min(1, sqrt(C)/norm(v, type = "2"))*v
+    if (norm(v - w, type = "2") < eps) {
+      break
+    } else {
+      w <- v
+    }
   }
   return(v)
 }
