6e87bcab84877e838d9e28e4604058dc1ac16562
define957 <zhangjiaqi957957@outlook.com>
2023-06-04 00:19:54 +0800

    update optimizer

--- R/optimizers.R
+++ R/optimizers.R
@@ -33,3 +33,33 @@ pegasos <- function(X, y, w, m, max.steps, fx, eps = 1e-5, ...) {
   }
   return(v)
 }
+
+#'  Conjugate Gradient Method for Solving Linear Equation Ax = b
+#'
+#' @author Zhang Jiaqi.
+#' @param A,b matrix of linear equation Ax = b (Since A is a PSD matrix).
+#' @param x initial point.
+#' @param max.steps the number of iterations to solve the optimization problem.
+#' @param eps the precision of the optimization algorithm.
+#' @param ... additional settings for the sub-gradient.
+#' @return return optimal solution.
+#' @export
+conjugate_gradient_method <- function(A, b, x, max.steps, eps = 1e-5, ...) {
+  rk <- b - A%*%x
+  pk <- rk
+  for (t in 1:max.steps) {
+    rk2 <- (t(rk) %*% rk)
+    alphak <-  as.numeric(rk2 / (t(pk) %*% A %*% pk))
+    x <- x + alphak*pk
+    rk_1 <- rk - alphak*(A%*%pk)
+    if (norm(rk_1, type = "2") < eps) {
+      cat("converge after", t, "steps", "\n")
+      break
+    } else {
+      betak <- as.numeric((t(rk_1) %*% rk_1) / rk2)
+      pk <- rk_1 + betak*pk
+    }
+    rk <- rk_1
+  }
+  return(x)
+}
