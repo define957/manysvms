6dca8bc5b20dde96772ce591069f02a553627e44
LiFeiH <lifh13398309563@163.com>
2023-07-16 19:35:18 +0800

    add pega function

--- R/optimizers.R
+++ R/optimizers.R
@@ -191,3 +191,43 @@ exp_decay <- function(lr, steps, s = 0.001, ...) {
   return(lr)
 }
 
+
+#'  Primal Estimated sub-Gradient SOlver for SVM (Pegasos)
+#'
+#' @author Zhang Jiaqi.
+#' @param X,y dataset and label.
+#' @param w initial point.
+#' @param m mini-batch size for pegasos solver.
+#' @param max.steps the number of iterations to solve the optimization problem.
+#' @param fx sub-gradient of objective function.
+#' @param pars parameters list for the sub-gradient.
+#' @param projection projection option.
+#' @param ... additional settings for the sub-gradient.
+#' @return return optimal solution.
+#' @references ${1:Pegasos: Primal Estimated sub-GrAdient SOlver for SVM}
+#' @export
+pega <- function(X, y, w, m, max.steps, fx, pars,
+                    projection = TRUE, ...) {
+  C <- pars$C
+  sample_seed <- list(...)$sample_seed
+  if (is.null(sample_seed) == FALSE) {
+    set.seed(sample_seed)
+  }
+  nx <- nrow(X)
+  px <- ncol(X)
+  for (t in 1:max.steps) {
+    At <- sample(nx, m)
+    xm <- X[At, ]
+    xm <- X[At, ]
+    dim(xm) <- c(m, px)
+    ym <- as.matrix(y[At])
+    # update parameter
+    dF <- fx(xm, ym, w, pars, At = At)
+    w <- w - (1/C*t)*dF
+    if (projection == TRUE) {
+      w <- min(1, sqrt(C)/norm(w, type = "2"))*w
+    }
+  }
+  return(w)
+}
+
