5a1b4d9200ea8aeb18b84c880a3001319d338f9f
define/957 <zhangjiaqi957957@outlook.com>
2023-12-22 20:07:26 +0800

    add hinge_tsvr

---
+++ R/hinge_tsvr.R
@@ -0,0 +1,116 @@
+hinge_tsvr_dual_solver <- function(KernelX, y, C1, C2, epsilon1, epsilon2,
+                                   eps, max.steps) {
+  nx <- nrow(KernelX)
+  np <- ncol(KernelX)
+  H <- KernelX
+  HTH_inv_H <- solve(t(H) %*% H + diag(1e-7, np), t(H))
+  dualH<- H %*% HTH_inv_H
+
+  f <- as.matrix(y - epsilon1)
+  h <- as.matrix(y + epsilon2)
+
+  q1 <- f - dualH %*% f
+  q2 <- dualH %*% h - h
+
+  lb <- matrix(0, nx, 1)
+  ub1 <- matrix(C1, nx, 1)
+  ub2 <- matrix(C2, nx, 1)
+  x0 <- lb
+  alphas <- clip_dcd_optimizer(dualH, q1, lb, ub1, eps, max.steps, x0)$x
+  gammas <- clip_dcd_optimizer(dualH, q2, lb, ub2, eps, max.steps, x0)$x
+
+  u1 <- HTH_inv_H %*% (f - alphas)
+  u2 <- HTH_inv_H %*% (h + gammas)
+  BaseDualHingeTSVRRegressor <- list("coef1" = as.matrix(u1),
+                                     "coef2" = as.matrix(u2))
+}
+
+#' Hinge Twin Support Vector Regression
+#'
+#' \code{hinge_tsvr} is an R implementation of Hinge-TSVR
+#'
+#' @author Zhang Jiaqi.
+#' @param X,y dataset and label.
+#' @param C1,C2 plenty term.
+#' @param epsilon1,epsilon2 parameter for epsilon tabe.
+#' @param kernel kernel function. The definitions of various kernel functions are as follows:
+#' \describe{
+#'     \item{linear:}{\eqn{u'v}{u'*v}}
+#'     \item{poly:}{\eqn{(\gamma u'v + coef0)^{degree}}{(gamma*u'*v + coef0)^degree}}
+#'     \item{rbf:}{\eqn{e^{(-\gamma |u-v|^2)}}{exp(-gamma*|u-v|^2)}}
+#' }
+#' @param gamma parameter for \code{'rbf'} and \code{'poly'} kernel. Default \code{gamma = 1/ncol(X)}.
+#' @param degree parameter for polynomial kernel, default: \code{degree = 3}.
+#' @param coef0 parameter for polynomial kernel,  default: \code{coef0 = 0}.
+#' @param eps the precision of the optimization algorithm.
+#' @param max.steps the number of iterations to solve the optimization problem.
+#' @param solver \code{"dual"} is available.
+#' @param fit_intercept if set \code{fit_intercept = TRUE},
+#'                      the function will evaluates intercept.
+#' @param randx parameter for reduce SVM, default \code{randx = 0.1}.
+#' @param ... unused parameters.
+#' @return return \code{TSVRClassifier} object.
+#' @export
+hinge_tsvr <- function(X, y, C1 = 1, C2 = C1,
+                       epsilon1 = 0.1, epsilon2 = epsilon1,
+                       kernel = c("linear", "rbf", "poly"),
+                       gamma = 1 / ncol(X), degree = 3, coef0 = 0,
+                       eps = 1e-14, max.steps = 4000,
+                       solver = c("dual"), fit_intercept = TRUE,
+                       randx = 0.1, ...) {
+  X <- as.matrix(X)
+  y <- as.matrix(y)
+  kernel <- match.arg(kernel)
+  solver <- match.arg(solver)
+  kso <- kernel_select_option(X, kernel, "primal", randx,
+                              gamma, degree, coef0)
+  KernelX <- kso$KernelX
+  Kw <- kso$KernelX[kso$sample_idx, ]
+  X <- kso$X
+  if (fit_intercept == TRUE) {
+    KernelX <- cbind(KernelX, 1)
+  }
+  if (solver == "dual") {
+    solver.res <- hinge_tsvr_dual_solver(KernelX, y, C1, C2,
+                                         epsilon1, epsilon2,
+                                         eps, max.steps)
+  }
+  TSVRegressor <- list("X" = X, "y" = y,
+                       "C1" = C1, "C2" = C2,
+                       "epsilon1" = epsilon1, "epsilon2" = epsilon2,
+                       "kernel" = kernel,
+                       "gamma" = gamma, "degree" = degree, "coef0" = coef0,
+                       "solver" = solver, "coef1" = solver.res$coef1,
+                       "coef2" = solver.res$coef2,
+                       "fit_intercept" = fit_intercept,
+                       "Kw" = Kw)
+  class(TSVRegressor) <- "TSVRegressor"
+  return(TSVRegressor)
+}
+
+
+#' Predict Method for Twin Support Vector Regression
+#'
+#' @author Zhang Jiaqi
+#' @param object a fitted object of class inheriting from \code{SVMClassifier}.
+#' @param X new data for predicting.
+#' @param ... unused parameter.
+#' @importFrom stats predict
+#' @export
+predict.TSVRegressor <- function(object, X, ...) {
+  X <- as.matrix(X)
+  if (object$kernel == "linear") {
+    KernelX <- X
+  } else {
+    KernelX <- kernel_function(X, object$X,
+                               kernel.type = object$kernel,
+                               gamma = object$gamma,
+                               degree = object$degree,
+                               coef0 = object$coef0)
+  }
+  if (object$fit_intercept == TRUE) {
+    KernelXe <- cbind(KernelX, 1)
+  }
+  f <- KernelXe %*% (object$coef1 + object$coef2) / 2
+  return(f)
+}
--- tests/eps_svr_synt_testing.R
+++ tests/eps_svr_synt_testing.R
@@ -3,7 +3,7 @@ library(manysvms)
 set.seed(1234)
 n <- 100
 x <- sort(rnorm(n))
-y <- 3*x + 1 + rnorm(n, 0, 0.1)
+y <- 3*x + 1 + rnorm(n, 0, 1)
 
 model <- ls_svr(x, y, C = 1, kernel = "linear", max.steps = 8000, solver = "primal", batch_size = 100,
                 optimizer = pegasos, projection = FALSE)
@@ -20,3 +20,16 @@ model <- bls_svr(x, y, C = 1, b = 1, kernel = "linear", max.steps = 8000)
 pred <- predict(model, x)
 plot(x, y, col = "red")
 lines(x, pred)
+
+model <- hinge_tsvr(x, y, C1 = 256,
+                    epsilon1 = 5, epsilon2 = 5,max.steps = 800000, eps = -0.1)
+c1 <- model$coef1
+c2 <- model$coef2
+plot(x, y, col = "red")
+pred <- predict(model, x)
+lines(x, pred)
+abline(c1[2], c1[1])
+abline(c2[2], c2[1])
+
+abline(c1[2] + 5, c1[1], col = "red")
+abline(c2[2] - 5, c2[1], col = "red")
