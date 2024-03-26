6299d198aaa5a093e48cbdf4da1f4fd06e8d2028
define957 <zhangjiaqi957957@outlook.com>
2024-03-26 22:38:53 +0800

    Add pin_tksvc

---
+++ R/pin_tksvc.R
@@ -0,0 +1,151 @@
+pin_tksvc_dual_solver <- function(KernelX, y, C, epsilon, tau,
+                                  class_set, class_num,
+                                  eps, max.steps) {
+  xp <- ncol(KernelX)
+  num_classifier <- class_num*(class_num - 1)/2
+  coef_pos <- matrix(0, xp, num_classifier)
+  coef_neg <- matrix(0, xp, num_classifier)
+
+  classifier_idx <- 1
+  for (i in 1:(class_num - 1)) {
+    for (j in (i + 1):class_num) {
+      idxPos <- which(y == class_set[i]) # positive samples
+      idxNeg <- which(y == class_set[j]) # negtive samples
+      idxRest <- which(y != class_set[i] & y != class_set[j]) # rest samples
+
+      # Hyperplane 1
+      H <- KernelX[idxPos, ]
+      G <- KernelX[idxNeg, ]
+      M <- KernelX[idxRest, ]
+      N <- rbind(G, M)
+
+      Hn <- nrow(H)
+      Gn <- nrow(G)
+      Mn <- nrow(M)
+
+      invHTH <- chol2inv(chol(t(H) %*% H + diag(1e-7, xp)))
+      dualH <- N %*% invHTH %*% t(N)
+      dualq <- rbind(matrix(1, Gn), matrix(1 - epsilon, Mn))
+      duallb <- rbind(matrix(-tau[1]*C[1], Gn), matrix(-tau[2]*C[2], Mn))
+      dualub <- rbind(matrix(C[1], Gn), matrix(C[2], Mn))
+      if (tau[1] == 0) {
+        u0_1 <- matrix(0, Gn)
+      } else {
+        u0_1 <- matrix((1 - tau[1]*C[1]) / 2, Gn)
+      }
+      if (tau[2] == 0) {
+        u0_2 <- matrix(0, Mn)
+      } else {
+        u0_2 <- matrix((1 - tau[2]*C[2]) / 2, Mn)
+      }
+      u0 <- rbind(u0_1, u0_2)
+      x <- clip_dcd_optimizer(dualH, dualq, duallb, dualub, eps, max.steps, u0)$x
+      coef1 <- -invHTH %*% t(N) %*% x
+
+      # Hyperplane 2
+      P <- rbind(H, M)
+      invGTG <- chol2inv(chol(t(G) %*% G + diag(1e-7, xp)))
+      dualH <- P %*% invGTG %*% t(P)
+      dualq <- rbind(matrix(1, Hn), matrix(1 - epsilon, Mn))
+      duallb <- rbind(matrix(-tau[3]*C[3], Hn), matrix(-tau[4]*C[4], Mn))
+      dualub <- rbind(matrix(C[3], Hn), matrix(C[4], Mn))
+      if (tau[3] == 0) {
+        u0_1 <- matrix(0, Gn)
+      } else {
+        u0_1 <- matrix((1 - tau[3]*C[3]) / 2, Gn)
+      }
+      if (tau[4] == 0) {
+        u0_2 <- matrix(0, Mn)
+      } else {
+        u0_2 <- matrix((1 - tau[4]*C[4]) / 2, Mn)
+      }
+      u0 <- rbind(u0_1, u0_2)
+      x <- clip_dcd_optimizer(dualH, dualq, duallb, dualub, eps, max.steps, u0)$x
+      coef2 <- invGTG %*% t(P) %*% x
+
+      coef_pos[, classifier_idx] <- coef1
+      coef_neg[, classifier_idx] <- coef2
+      classifier_idx <- classifier_idx + 1
+    }
+  }
+  BaseDualPinTKSVCClassifier <- list("coef_pos" = coef_pos,
+                                     "coef_neg" = coef_neg)
+  return(BaseDualPinTKSVCClassifier)
+}
+
+#' Hinge Twin Multi-Class Support Vector Machine
+#'
+#' \code{hinge_tksvc} is an R implementation of Hinge-TKSVC
+#'
+#' @author Zhang Jiaqi.
+#' @param X,y dataset and label.
+#' @param C plenty term.
+#' @param kernel kernel function. The definitions of various kernel functions are as follows:
+#' \describe{
+#'     \item{linear:}{\eqn{u'v}{u'*v}}
+#'     \item{poly:}{\eqn{(\gamma u'v + coef0)^{degree}}{(gamma*u'*v + coef0)^degree}}
+#'     \item{rbf:}{\eqn{e^{(-\gamma |u-v|^2)}}{exp(-gamma*|u-v|^2)}}
+#' }
+#' @param gamma parameter for \code{'rbf'} and \code{'poly'} kernel. Default \code{gamma = 1/ncol(X)}.
+#' @param degree parameter for polynomial kernel, default: \code{degree = 3}.
+#' @param coef0 parameter for polynomial kernel,  default: \code{coef0 = 0}.
+#' @param epsilon rest class parameter.
+#' @param tau parameter for pinball loss function.
+#' @param eps the precision of the optimization algorithm.
+#' @param max.steps the number of iterations to solve the optimization problem.
+#' @param solver \code{"dual"} is available.
+#' @param fit_intercept if set \code{fit_intercept = TRUE},
+#'                      the function will evaluates intercept.
+#' @param randx parameter for reduce SVM, default \code{randx = 0.1}.
+#' @return return \code{HingeSVMClassifier} object.
+#' @export
+pin_tksvc <- function(X, y, C = 1,
+                      kernel = c("linear", "rbf", "poly"),
+                      gamma = 1 / ncol(X), degree = 3, coef0 = 0,
+                      epsilon = 0.1, tau = 0.1,
+                      eps = 1e-5, max.steps = 5000,
+                      solver = c("dual"), fit_intercept = TRUE,
+                      randx = 1) {
+  C <- as.vector(C)
+  X <- as.matrix(X)
+  y <- as.matrix(y)
+  class_set <- sort(unique(y))
+  class_num <- length(class_set)
+  if (length(C) == 1) {
+    C <- matrix(C, 4)
+  } else if (length(C) == 2) {
+    C <- c(C, C)
+  } else if (length(C) != 4) {
+    stop("length(C) should equal to 1, 2 or 4!")
+  }
+  if (length(tau) == 1) {
+    tau <- matrix(tau, 4)
+  } else if (length(tau) == 2) {
+    tau <- c(tau, tau)
+  } else if (length(tau) != 4) {
+    stop("length(tau) should equal to 1, 2 or 4!")
+  }
+  kernel <- match.arg(kernel)
+  solver <- match.arg(solver)
+  kso <- kernel_select_option(X, kernel, "primal", randx,
+                              gamma, degree, coef0)
+  KernelX <- kso$KernelX
+  X <- kso$X
+  if (fit_intercept == TRUE) {
+    KernelX <- cbind(KernelX, 1)
+  }
+  if (solver == "dual") {
+    solver.res <- pin_tksvc_dual_solver(KernelX, y, C, epsilon, tau,
+                                        class_set, class_num, eps, max.steps)
+  }
+  TKSVCClassifier <- list("X" = X, "y" = y, "class_set" = class_set,
+                          "class_num" = class_num,
+                          "C" = C, "kernel" = kernel,
+                          "gamma" = gamma, "degree" = degree, "coef0" = coef0,
+                          "epsilon" = epsilon,
+                          "solver" = solver, "coef_pos" = solver.res$coef_pos,
+                          "coef_neg" = solver.res$coef_neg,
+                          "fit_intercept" = fit_intercept)
+  class(TKSVCClassifier) <- "TKSVCClassifier"
+  return(TKSVCClassifier)
+}
