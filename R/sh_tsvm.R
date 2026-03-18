c5162f738b9c895e057fe333abd99105fac76eff
define957 <zhangjiaqi957957@outlook.com>
2026-03-18 17:11:03 +0800

    Update sh_tsvm

--- R/sh_tsvm.R
+++ R/sh_tsvm.R
@@ -1,32 +1,36 @@
 sh_tsvm_dual_solver <- function(KernelX, idx, C1, C2, eps, max.steps) {
-  H <- KernelX[-idx, ]
-  G <- KernelX[idx, ]
-  xn <- nrow(KernelX)
-  xp <- ncol(KernelX)
+
+  H  <- KernelX[-idx, , drop = FALSE]
+  G  <- KernelX[idx, , drop = FALSE]
   Hn <- nrow(H)
-  Gn <- xn - Hn
+  Gn <- nrow(G)
 
-  invHTH_GT <- cholsolve(t(H) %*% H + diag(1e-7, xp), t(G))
-  dualH <- G %*% invHTH_GT + diag(1/C1, Gn)
-  dualq1 <- matrix(1, Gn)
-  duallb1 <- matrix(0, Gn)
-  dualub1 <- matrix(Inf, Gn)
-  u0 <- matrix(0, Gn)
-  a <- clip_dcd_optimizer(dualH, dualq1, duallb1, dualub1,
-                          eps, max.steps, u0)$x
-  u <- -invHTH_GT %*% a
+  HTH        <- t(H) %*% H
+  diag(HTH)  <- diag(HTH) + 1e-7
+  invHTH_GT  <- cholsolve(HTH, t(G))
+  dualH1     <- G %*% invHTH_GT + diag(1/C1, Gn)
+  dualq1     <- matrix(1, Gn)
+  duallb1    <- matrix(0, Gn)
+  dualub1    <- matrix(Inf, Gn)
+  u01        <- matrix(0, Gn)
+  dual_coef1 <- clip_dcd_optimizer(dualH1, dualq1, duallb1, dualub1,
+                                   eps, max.steps, u01)$x
+  coef1      <- -invHTH_GT %*% dual_coef1
 
-  invGTG_HT <- cholsolve(t(G) %*% G + diag(1e-7, xp), t(H))
-  dualH <- H %*% invGTG_HT + diag(1/C2, Hn)
-  dualq2 <- matrix(1, Hn)
-  duallb2 <- matrix(0, Hn)
-  dualub2 <- matrix(Inf, Hn)
-  u0 <- matrix(0, Hn)
-  g <- clip_dcd_optimizer(dualH, dualq2, duallb2, dualub2,
-                          eps, max.steps, u0)$x
-  v <- invGTG_HT %*% g
-  BaseDualSquaredHingeTSVMClassifier <- list("coef1" = as.matrix(u),
-                                             "coef2" = as.matrix(v))
+  GTG        <- t(G) %*% G
+  diag(GTG)  <- diag(GTG) + 1e-7
+  invGTG_HT  <- cholsolve(GTG, t(H))
+  dualH2     <- H %*% invGTG_HT + diag(1/C2, Hn)
+  dualq2     <- matrix(1, Hn)
+  duallb2    <- matrix(0, Hn)
+  dualub2    <- matrix(Inf, Hn)
+  u02        <- matrix(0, Hn)
+  dual_coef2 <- clip_dcd_optimizer(dualH2, dualq2, duallb2, dualub2,
+                                   eps, max.steps, u02)$x
+  coef2      <- invGTG_HT %*% dual_coef2
+
+  BaseDualSquaredHingeTSVMClassifier <- list("coef1" = as.matrix(coef1),
+                                             "coef2" = as.matrix(coef2))
   return(BaseDualSquaredHingeTSVMClassifier)
 }
 
@@ -58,20 +62,21 @@ sh_tsvm <- function(X, y, C1 = 1, C2 = 1,
                     gamma = 1 / ncol(X), degree = 3, coef0 = 0,
                     eps = 1e-5, max.steps = 4000,
                     fit_intercept = TRUE, reduce_set = NULL) {
-  X <- as.matrix(X)
-  y <- as.matrix(y)
-
+  X         <- as.matrix(X)
+  y         <- as.matrix(y)
   class_set <- sort(unique(y))
-  idx <- which(y == class_set[1])
-  y[idx] <- -1
-  y[-idx] <- 1
-  y <- as.matrix(as.numeric(y))
-  if (length(class_set) > 2) {
-    stop("The number of class should less 2!")
+  idx       <- which(y == class_set[1])
+  y[idx]    <- -1
+  y[-idx]   <- 1
+  y         <- as.matrix(as.numeric(y))
+
+  if (length(class_set) != 2) {
+    stop("This model only supports binary classification; y must have exactly two classes")
   }
 
-  kernel <- match.arg(kernel)
+  kernel  <- match.arg(kernel)
   KernelR <- NULL
+
   if (kernel != "linear") {
     kso <- kernel_select_option_(X, kernel, reduce_set, gamma, degree, coef0)
     KernelX <- kso$KernelX
@@ -85,25 +90,25 @@ sh_tsvm <- function(X, y, C1 = 1, C2 = 1,
   }
   solver.res <- sh_tsvm_dual_solver(KernelX, idx, C1, C2, eps, max.steps)
 
-  model_specs = list("X" = X, "y" = y,
-                     "C1" = C1, "C2" = C2,
-                     "fit_intercept" = fit_intercept,
-                     "class_set" = class_set)
-  model_coef = list("coef1" = solver.res$coef1,
-                    "coef2" = solver.res$coef2)
-  kernel_config = list("kernel" = kernel,
-                       "gamma" = gamma,
-                       "degree" = degree,
-                       "coef0" = coef0,
-                       "reduce_set" = reduce_set,
-                       "KernelR" = KernelR,
-                       "KernelX" = KernelX[, 1:kxp, drop = FALSE])
-  w_norm <- get_coef_norm(kernel_config, model_coef)
+  model_specs   <- list("X" = X, "y" = y,
+                        "C1" = C1, "C2" = C2,
+                        "fit_intercept" = fit_intercept,
+                        "class_set" = class_set)
+  model_coef    <- list("coef1" = solver.res$coef1,
+                        "coef2" = solver.res$coef2)
+  kernel_config <- list("kernel" = kernel,
+                        "gamma"  = gamma,
+                        "degree" = degree,
+                        "coef0" = coef0,
+                        "reduce_set" = reduce_set,
+                        "KernelR" = KernelR,
+                        "KernelX" = KernelX[, 1:kxp, drop = FALSE])
+  w_norm        <- get_coef_norm(kernel_config, model_coef)
   kernel_config$w_norm <- w_norm
-  TSVMClassifier <- structure(
-    list("model_specs" = model_specs,
-         "model_coef" = model_coef,
-         "kernel_config" = kernel_config),
-    "class" = "TSVMClassifier")
+
+  TSVMClassifier <- structure(list("model_specs" = model_specs,
+                                   "model_coef" = model_coef,
+                                   "kernel_config" = kernel_config),
+                                   "class" = "TSVMClassifier")
   return(TSVMClassifier)
 }
