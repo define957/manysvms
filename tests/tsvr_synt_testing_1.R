8e0da8b498866e81689c0e8ae473e8a4dfa59fef
define957 <zhangjiaqi957957@outlook.com>
2026-04-29 15:36:03 +0800

    Update test file

--- tests/tsvr_synt_testing_1.R
+++ tests/tsvr_synt_testing_1.R
@@ -7,7 +7,7 @@ y <- 3*x + 1 + rnorm(n, 0, 3)
 
 epsilon <- 1.5
 model <- hinge_tsvr(x, y, kernel = "linear", epsilon1 = epsilon, max.steps = 80000,
-                    C1 = 1, eps = 0)
+                    C1 = 0, eps = 0)
 plot(model)
 mean_squared_error(y, predict(model, x))
 
@@ -16,7 +16,13 @@ model <- hinge_eps_tsvr(x, y, kernel = "linear", epsilon1 = epsilon, max.steps =
 plot(model)
 mean_squared_error(y, predict(model, x))
 
+model <- sh_tsvr(x, y, kernel = "linear", epsilon1 = epsilon, max.steps = 80000,
+                 C1 = 1, eps = 0)
+plot(model)
+mean_squared_error(y, predict(model, x))
+
 model <- sh_eps_tsvr(x, y, kernel = "linear", epsilon1 = epsilon, max.steps = 80000,
                         C1 = 1, eps = 0)
 plot(model)
 mean_squared_error(y, predict(model, x))
+
